7ec24b5b39e01ca780b9b343f4086802dcd5029f
Legonius <emoinblackzone@gmail.com>
2024-12-26 00:01:52 +0630

    FlatList

--- components/pokemon/AllPokemon.js
+++ components/pokemon/AllPokemon.js
@@ -1,4 +1,11 @@
-import { View, ScrollView, Text, StyleSheet, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Pressable,
+  FlatList,
+  SectionList,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { pokemonApi } from "../../constants";
 
@@ -29,25 +36,61 @@ export default function AllPokemon({ setPkmId }) {
   }, []); // Dependency array should be empty to run the effect only once
 
   return (
-    <ScrollView style={styles.container}>
-      {pokemons.length > 0 ? (
-        pokemons.slice(0, 200).map((pkm) => (
-          <Pressable key={pkm.id} onPress={() => handlePress(pkm.id)}>
+    // <ScrollView style={styles.container}>
+    //   {pokemons.length > 0 ? (
+    //     pokemons.slice(0, 200).map((pkm) => (
+    // <Pressable key={pkm.id} onPress={() => handlePress(pkm.id)}>
+    //   <Text
+    //     style={[
+    //       styles.text,
+    //       { textAlign: pkm.id % 2 !== 0 ? "left" : "right" },
+    //     ]}
+    //   >
+    //     {pkm.name}
+    //   </Text>
+    //   <View style={styles.line} />
+    // </Pressable>
+    //     ))
+    //   ) : (
+    //     <Text style={styles.text}>Loading...</Text>
+    //   )}
+    // </ScrollView>
+
+    // ============= FlatList ==============
+
+    <View style={styles.container}>
+      {/* {Better to use FlatList than ScrollView to show so many list to optimize app} */}
+      <FlatList
+        data={pokemons}
+        renderItem={({ item }) => (
+          <Pressable
+            style={styles.container1}
+            onPress={() => handlePress(item.id)}
+          >
             <Text
               style={[
                 styles.text,
-                { textAlign: pkm.id % 2 !== 0 ? "left" : "right" },
+                { textAlign: item.id % 2 !== 0 ? "left" : "right" },
               ]}
             >
-              {pkm.name}
+              {item.name}
             </Text>
-            <View style={styles.line} />
+            {/* <View style={styles.line} /> */}
           </Pressable>
-        ))
-      ) : (
-        <Text style={styles.text}>Loading...</Text>
-      )}
-    </ScrollView>
+        )}
+        ListEmptyComponent={<Text style={styles.text}>No items found.</Text>}
+        ListHeaderComponent={
+          <Text style={styles.header}>All Pokemon List</Text>
+        }
+        ListFooterComponent={
+          <Text style={styles.header}>No More Pokemons.</Text>
+        }
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={<View style={styles.seperator} />}
+        h
+        // horizontal
+      />
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -67,5 +110,21 @@ const styles = StyleSheet.create({
     backgroundColor: "gray",
     opacity: 0.5,
     marginVertical: 10,
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: 700,
+    color: "rgb(237, 240, 19)",
+    textAlign: "center",
+    marginBottom: 15,
+  },
+  seperator: {
+    height: 20,
+  },
+  container1: {
+    borderColor: "white",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 15,
   },
 });
