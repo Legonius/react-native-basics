7d8b8b0b49a8da8472f93c075fdc99de135ba7b5
Legonius <emoinblackzone@gmail.com>
2024-12-30 14:28:04 +0630

    Tab Navigation

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 import Pokemon from "./lessons/pokemon/pokemon";
 import StackNavigation from "./lessons/navigation/StackNavigation";
 import NavigationDrawer from "./lessons/NavigationDrawer";
+import TabNavi from "./lessons/TabNavi";
 
 export default function App() {
   const [isPokemon, setIsPokemon] = useState(false);
   // return isPokemon ? <Pokemon /> : <StackNavigation />;
-  return isPokemon ? <Pokemon /> : <NavigationDrawer />;
+  return isPokemon ? <Pokemon /> : <TabNavi />;
 }
---
+++ lessons/TabNavi.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import dashboard from "./screens/dashboard";
+import Settings from "./screens/Settings";
+import Fontisto from "@expo/vector-icons/Fontisto";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+export default function TabNavi() {
+  const Tab = createBottomTabNavigator();
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarLabelPosition: "beside-icon",
+          tabBarStyle: {
+            backgroundColor: "wheat",
+          },
+          tabBarLabelStyle: {
+            fontSize: 20,
+          },
+          headerShown: false,
+        }}
+      >
+        <Tab.Screen
+          options={{
+            tabBarBadge: 3,
+            tabBarIcon: () => <Fontisto name="home" size={24} color="black" />,
+          }}
+          name="Home"
+          component={dashboard}
+        />
+        <Tab.Screen
+          options={{
+            tabBarIcon: () => (
+              <MaterialIcons name="settings" size={24} color="black" />
+            ),
+            title: "Title Setting",
+          }}
+          name="Setting"
+          component={Settings}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
